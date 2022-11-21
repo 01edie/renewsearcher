562f9734a874c50fc9ab99f1b9a61a155f80bb7f
BISWAJIT BOSE <97077410+01edie@users.noreply.github.com>
2022-11-21 09:37:54 +0530

    minor change for netlify

--- src/components/LandingFeatures.js
+++ src/components/LandingFeatures.js
@@ -1,9 +1,9 @@
-import { Grid, } from "@mui/material";
+import { Grid, Box} from "@mui/material";
 import React, {  } from "react";
 
 const LandingFeatures = () => {
-  return <Grid item xs={12} sm={4}>
-    Features will be added here
+  return <Grid item xs={12} sm={4} >
+    <Box m='50px 5px 10px 10px'> Features will be added here</Box>
   </Grid>;
 };
 
